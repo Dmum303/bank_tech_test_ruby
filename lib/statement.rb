5833ff99ea1237a25a1004ea0449480c6d7bdbbc
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-12-14 15:06:29 +0000

    sort by date function made

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,11 +2,17 @@ class Statement
   def initialize
     $transactions = []
   end
+
   def add(transaction)
     $transactions.push(transaction)
   end
 
   def transaction_list
     $transactions
+  end
+
+  def sort_by_date
+    $transactions =
+      $transactions.sort_by { |transaction| transaction.date_to_object }
   end
 end
--- spec/statement_unit_spec.rb
+++ spec/statement_unit_spec.rb
@@ -2,27 +2,74 @@ require_relative "../lib/statement"
 require "date"
 
 RSpec.describe Statement do
-  #context
-  it "checks add and transaction list methods" do
-    #   transaction_1_credit =
-    #     object_double(
-    #       :fake_transaction,
-    #       show_amount: 300,
-    #       show_string_date: "03-04-2022",
-    #       date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
-    #       show_transaction_type: "credit"
-    #     ),
-    #     transaction_2_credit =
-    #       object_double(
-    #         :fake_ransaction_2,
-    #         show_amount: 50.50,
-    #         show_string_date: "01-10-2021",
-    #         date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
-    #         show_transaction_type: "credit"
-    #       )
-    fake_object = double :fake_object
-    statement = Statement.new
-    statement.add(fake_object)
-    expect(statement.transaction_list).to eq [fake_object]
+  context "Check eck" do
+    it "Returns the empty transactions array" do
+      statement = Statement.new
+      expect(statement.transaction_list).to eq []
+    end
+
+    it "checks add and transaction list methods" do
+      fake_object = double :fake_object
+      statement = Statement.new
+      statement.add(fake_object)
+      expect(statement.transaction_list).to eq [fake_object]
+    end
+
+    it "checks several additions" do
+      fake_object = double :fake_object
+      fake_object_2 = double :fake_object_2
+      statement = Statement.new
+      statement.add(fake_object)
+      statement.add(fake_object_2)
+      expect(statement.transaction_list.length).to eq 2
+    end
+
+    it "Sorts transactions array by earliest date" do
+      fake_transaction =
+        double :fake_transaction,
+               date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y")
+      fake_transaction_2 =
+        double :fake_transaction_2,
+               date_to_object: Date.strptime("03-04-2021", "%d-%m-%Y")
+      fake_transaction_3 =
+        double :fake_transaction_3,
+               date_to_object: Date.strptime("03-04-2023", "%d-%m-%Y")
+      statement = Statement.new
+      statement.add(fake_transaction)
+      statement.add(fake_transaction_2)
+      statement.add(fake_transaction_3)
+      statement.sort_by_date
+      expect(statement.transaction_list.length).to eq 3
+      expect(statement.transaction_list[0]).to eq fake_transaction_2
+      expect(statement.transaction_list[2]).to eq fake_transaction_3
+    end
+
+    it "Formats object into string" do
+      fake_transaction =
+        double :fake_transaction,
+               show_amount: 300,
+               show_string_date: "03-04-2022",
+               date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
+               show_transaction_type: "credit"
+      statement = Statement.new
+      expect(statement.format_to_string(fake_transaction)).to eq
+    end
   end
 end
+
+#   transaction_1_credit =
+#     object_double(
+#       :fake_transaction,
+#       show_amount: 300,
+#       show_string_date: "03-04-2022",
+#       date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
+#       show_transaction_type: "credit"
+#     ),
+#     transaction_2_credit =
+#       object_double(
+#         :fake_ransaction_2,
+#         show_amount: 50.50,
+#         show_string_date: "01-10-2021",
+#         date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
+#         show_transaction_type: "credit"
+#       )
