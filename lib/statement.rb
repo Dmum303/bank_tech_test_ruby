e6fcec4f95d0ea6f4707250f066f9603f334e523
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-12-14 14:18:39 +0000

    first statement methods tested

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,2 +1,12 @@
 class Statement
+  def initialize
+    $transactions = []
+  end
+  def add(transaction)
+    $transactions.push(transaction)
+  end
+
+  def transaction_list
+    $transactions
+  end
 end
