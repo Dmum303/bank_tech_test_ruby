a5f3319709d89016fad234a3bb80fac7b33c4d09
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-12-15 17:53:29 +0000

    Debug integration test

---
+++ spec/statement_integration_spec.rb
@@ -0,0 +1,42 @@
+require_relative "../lib/statement"
+require_relative "../lib/transaction"
+
+RSpec.describe Statement do
+  context "a context" do
+    it "Produces a statement with one transaction" do
+      transaction = Transaction.new(450, "12-12-2022", "credit")
+      transaction.check_all_data_input
+      statement = Statement.new
+      statement.add(transaction)
+      expect(
+        statement.return_statement
+      ).to eq "date || credit || debit || balance\n12/12/2022 || 450.00 || || 450.00"
+    end
+
+    it "Produces a statement with one transaction" do
+      transaction = Transaction.new(300, "01-12-2021", "debit")
+      transaction.check_all_data_input
+      statement = Statement.new(1000)
+      statement.add(transaction)
+      expect(
+        statement.return_statement
+      ).to eq "date || credit || debit || balance\n01/12/2021 || || 300.00 || 700.00"
+    end
+
+    it "Produces a statement with several transactions" do
+      transaction = Transaction.new(500, "14-01-2023", "debit")
+      transaction_2 = Transaction.new(2000, "13-01-2023", "credit")
+      transaction_3 = Transaction.new(1000, "10-01-2023", "credit")
+      transaction.check_all_data_input
+      statement = Statement.new(3000)
+      statement.add(transaction)
+      statement.add(transaction_2)
+      statement.add(transaction_3)
+      expect(
+        statement.return_statement
+      ).to eq "date || credit || debit || balance\n01/12/2021 || || 300.00 || 700.00"
+    end
+  end
+end
+
+#  Have noticed that the dates are in the oposite order to what I have and there's a bug
--- spec/statement_unit_spec.rb
+++ spec/statement_unit_spec.rb
@@ -2,7 +2,7 @@ require_relative "../lib/statement"
 require "date"
 
 RSpec.describe Statement do
-  context "Check eck" do
+  context "Transaction array testing" do
     it "Returns the empty transactions array" do
       statement = Statement.new
       expect(statement.transaction_list).to eq []
@@ -43,7 +43,9 @@ RSpec.describe Statement do
       expect(statement.transaction_list[0]).to eq fake_transaction_2
       expect(statement.transaction_list[2]).to eq fake_transaction_3
     end
+  end
 
+  context "Running total testing" do
     it "Running total adds one transaction and returns total amount input" do
       fake_transaction =
         double :fake_transaction,
@@ -69,6 +71,15 @@ RSpec.describe Statement do
       expect(statement.running_total(fake_transaction_2)).to eq 510
     end
 
+    it "Can add a optional starting bank balance" do
+      statement = Statement.new(2000)
+      expect(statement.show_balance).to eq 2000
+      statement_2 = Statement.new()
+      expect(statement_2.show_balance).to eq 0
+    end
+  end
+
+  context "Object to String testing" do
     it "Formats transaction object into string" do
       fake_transaction =
         double :fake_transaction,
@@ -106,82 +117,77 @@ RSpec.describe Statement do
     end
   end
 
-  it "Can add a optional starting bank balance" do
-    statement = Statement.new(2000)
-    expect(statement.show_balance).to eq 2000
-    statement_2 = Statement.new()
-    expect(statement_2.show_balance).to eq 0
-  end
+  context "Construct and return complete statement" do
+    it "Takes array of transaction objects and converts to array of strings" do
+      fake_transaction =
+        double :fake_transaction,
+               show_amount: 300,
+               show_string_date: "03-04-2022",
+               date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
+               show_transaction_type: "credit"
 
-  it "Takes array of transaction objects and converts to array of strings" do
-    fake_transaction =
-      double :fake_transaction,
-             show_amount: 300,
-             show_string_date: "03-04-2022",
-             date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
-             show_transaction_type: "credit"
+      statement = Statement.new
+      statement.add(fake_transaction)
+      expect(
+        statement.produce_statement[0]
+      ).to eq "03/04/2022 || 300.00 || || 300.00"
+    end
 
-    statement = Statement.new
-    statement.add(fake_transaction)
-    expect(
-      statement.produce_statement[0]
-    ).to eq "03/04/2022 || 300.00 || || 300.00"
-  end
+    it "Takes array of transaction objects and converts to array of strings" do
+      fake_transaction =
+        double :fake_transaction,
+               show_amount: 300,
+               show_string_date: "03-04-2022",
+               date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
+               show_transaction_type: "credit"
+      fake_transaction_2 =
+        double :fake_transaction_2,
+               show_amount: 50.50,
+               show_string_date: "01-10-2021",
+               date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
+               show_transaction_type: "debit"
+      statement = Statement.new(250.23)
+      statement.add(fake_transaction)
+      statement.add(fake_transaction_2)
+      statement.add(fake_transaction_2)
+      expect(
+        statement.produce_statement[2]
+      ).to eq "03/04/2022 || 300.00 || || 449.23"
+    end
 
-  it "Takes array of transaction objects and converts to array of strings" do
-    fake_transaction =
-      double :fake_transaction,
-             show_amount: 300,
-             show_string_date: "03-04-2022",
-             date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
-             show_transaction_type: "credit"
-    fake_transaction_2 =
-      double :fake_transaction_2,
-             show_amount: 50.50,
-             show_string_date: "01-10-2021",
-             date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
-             show_transaction_type: "debit"
-    statement = Statement.new(250.23)
-    statement.add(fake_transaction)
-    statement.add(fake_transaction_2)
-    statement.add(fake_transaction_2)
-    expect(
-      statement.produce_statement[2]
-    ).to eq "03/04/2022 || 300.00 || || 449.23"
-  end
+    it "Returns statement as string with correct header" do
+      fake_transaction_2 =
+        double :fake_transaction_2,
+               show_amount: 50.50,
+               show_string_date: "01-10-2021",
+               date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
+               show_transaction_type: "debit"
+      statement = Statement.new
+      statement.add(fake_transaction_2)
+      expect(
+        statement.return_statement
+      ).to eq "date || credit || debit || balance\n01/10/2021 || || 50.50 || -50.50"
+    end
 
-  it "Returns statement as string with correct header" do
-    fake_transaction_2 =
-      double :fake_transaction_2,
-             show_amount: 50.50,
-             show_string_date: "01-10-2021",
-             date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
-             show_transaction_type: "debit"
-    statement = Statement.new
-    statement.add(fake_transaction_2)
-    expect(
-      statement.return_statement
-    ).to eq "date || credit || debit || balance\n01/10/2021 || || 50.50 || -50.50"
-  end
-
-  it "Returns statement as string with correct header" do
-    fake_transaction =
-      double :fake_transaction,
-             show_amount: 300,
-             show_string_date: "03-04-2022",
-             date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
-             show_transaction_type: "credit"
-    fake_transaction_2 =
-      double :fake_transaction_2,
-             show_amount: 50.50,
-             show_string_date: "01-10-2021",
-             date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
-             show_transaction_type: "debit"
-    statement = Statement.new(1000)
-    statement.add(fake_transaction_2)
-    statement.add(fake_transaction)
-    expect(
-      statement.return_statement
-    ).to eq "date || credit || debit || balance\n01/10/2021 || || 50.50 || 949.50\n03/04/2022 || 300.00 || || 1249.50"
+    it "Returns statement as string with correct header" do
+      fake_transaction =
+        double :fake_transaction,
+               show_amount: 300,
+               show_string_date: "03-04-2022",
+               date_to_object: Date.strptime("03-04-2022", "%d-%m-%Y"),
+               show_transaction_type: "credit"
+      fake_transaction_2 =
+        double :fake_transaction_2,
+               show_amount: 50.50,
+               show_string_date: "01-10-2021",
+               date_to_object: Date.strptime("01-10-2021", "%d-%m-%Y"),
+               show_transaction_type: "debit"
+      statement = Statement.new(1000)
+      statement.add(fake_transaction_2)
+      statement.add(fake_transaction)
+      expect(
+        statement.return_statement
+      ).to eq "date || credit || debit || balance\n01/10/2021 || || 50.50 || 949.50\n03/04/2022 || 300.00 || || 1249.50"
+    end
   end
 end
